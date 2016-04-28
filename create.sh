2d1d248f26d295508b0e1ce86a9ab04594da92b2
Josh Mandel <jmandel@med.harvard.edu>
2016-04-28 12:17:14 -0400

    index.html

--- create.sh
+++ create.sh
@@ -1,3 +1,4 @@
 wget https://s3.amazonaws.com/public-inspection.federalregister.gov/2016-10032.pdf
 pdftohtml  2016-10032.pdf -s -noframes macra.html
 sed -i "8i <script async src='//genius.codes'></script>" macra.html
+mv macra.html index.html
